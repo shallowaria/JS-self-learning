db3f53cada4b8a2d8128d33b31f433dd889ba646
shallowaria <shallowaria@gmail.com>
2025-05-07 16:10:02 +0800

    arrayloop

--- JavaScript笔记/javascriptcourse/11.Arrays and Loops/script/11-todo-list.js
+++ JavaScript笔记/javascriptcourse/11.Arrays and Loops/script/11-todo-list.js
@@ -1,25 +1,37 @@
-const todoList = [];
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2022-12-22'
+},
+{
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+}];
 
 renderTodoList();
 
 function renderTodoList() {
     let todoListHTML = '';
-// Generation the HTML
-for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
-    todoListHTML += html;
-}
+    // Generation the HTML
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;简写↓
+        const { name, dueDate } = todoObject;
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="todoList.splice(${i}, 1); renderTodoList();" class="delete-todo-button">Delete</button>
+    `;
+        todoListHTML += html;
+    }
 
-console.log(todoListHTML);
-
-document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
 document.querySelector('.js-name-input').addEventListener('keydown', enterlist);
 
 function enterlist(event) {
-    if(event.key === 'Enter') {
+    if (event.key === 'Enter') {
         addTodo();
     }
 }
@@ -27,9 +39,16 @@ function enterlist(event) {
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
-    
-    todoList.push(name);
-    console.log(todoList);
+
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        //name: name,
+        //dueDate: dueDate 简写↓
+        name,
+        dueDate
+    });
 
     inputElement.value = '';
 
