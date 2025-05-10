0720ff7722be80c92eb93d1bda5cbc38e9bde8b2
shallowaria <shallowaria@gmail.com>
2025-05-10 21:26:08 +0800

    12d

---
+++ JavaScript笔记/javascriptcourse/12.advanced-functions.html/script/12d.js
@@ -0,0 +1,184 @@
+// 2.调用localStorage存储
+let score = JSON.parse(localStorage.getItem('score')) ||
+{
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+updateScoreElement();
+
+/*
+if (!score) {
+    score = {
+        wins: 0,
+        losses: 0,
+        ties: 0
+    };
+}
+*/
+function resetScore() {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement();
+}
+
+function pickComputerMove() {
+    const randomNumber = Math.random();
+
+    let computerMove = '';
+    // 局部函数不能在局外作用
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = 'rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = 'paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+        computerMove = 'scissors';
+    }
+    // 返回语句,可添加任何东西，且立即结束语句
+    return computerMove;
+}
+
+document.querySelector('.js-auto-play-button').addEventListener('click', () => {
+    autoPlay();
+});
+
+document.querySelector('.js-reset-score-button').addEventListener('click', () => {
+    showResetConfirmation();
+});
+
+// 自动游玩与停止
+let isAutoPlaying = false;
+let intervalId;
+
+//const autoPlay = () => {
+
+//};
+// 命名函数用传统，更易阅读且有变量提升
+function autoPlay() {
+    if (!isAutoPlaying) {
+        document.querySelector('.js-auto-play-button').innerHTML = 'Stop Playing';
+        intervalId = setInterval(() => {
+            const playerMove = pickComputerMove();
+            playGame(playerMove);
+        }, 1000);
+        isAutoPlaying = true;
+    } else {
+        document.querySelector('.js-auto-play-button').innerHTML = 'Auto Play';
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+    }
+}
+
+//不能直接写playGame('rock'),否则会立即执行，直接返回结果,而此处要求的是一个函数，需要等用户点击按钮时才执行
+//不需要参数可以直接传函数名而不用箭头函数
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+    playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+    playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+    playGame('scissors');
+});
+
+// 按下键盘时会保存事件到箭头函数的event并运行函数
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key === 's') {
+        playGame('scissors');
+    } else if (event.key === 'a') {
+        autoPlay();
+    } else if (event.key === 'Backspace') {
+        showResetConfirmation();
+    }
+});
+
+function playGame(playerMove) {
+    // return到这里的pickComputerMove中再赋值到此处的computerMove
+    const computerMove = pickComputerMove();
+
+    let result = '';
+
+    if (playerMove === 'rock') {
+        if (computerMove === 'rock') {
+            result = 'Tie.';
+        } else if (computerMove === 'paper') {
+            result = 'You lose.';
+        } else if (computerMove === 'scissors') {
+            result = 'You win!';
+        }
+    }
+    if (playerMove === 'paper') {
+        if (computerMove === 'rock') {
+            result = 'You win!';
+        } else if (computerMove === 'paper') {
+            result = 'Tie.';
+        } else if (computerMove === 'scissors') {
+            result = 'You lose.';
+        }
+    }
+    if (playerMove === 'scissors') {
+        if (computerMove === 'rock') {
+            result = 'You lose.';
+        } else if (computerMove === 'paper') {
+            result = 'You win!';
+        } else if (computerMove === 'scissors') {
+            result = 'Tie.';
+        }
+    }
+
+    if (result === 'You win!') {
+        score.wins += 1;
+    } else if (result === 'You lose.') {
+        score.losses += 1;
+    } else if (result === 'Tie.') {
+        score.ties += 1;
+    }
+    // 1.只支持存储字符串
+    localStorage.setItem('score', JSON.stringify(score));
+
+    updateScoreElement()
+
+    document.querySelector('.js-result').innerHTML = result;
+    document.querySelector('.js-moves').innerHTML = `You 
+        <img src="../images/${playerMove}-emoji.png" alt="" class="move-icon">
+        <img src="../images/${computerMove}-emoji.png" alt="" class="move-icon">
+        Computer`;
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score')
+        .innerHTML = `Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`;
+}
+
+function showResetConfirmation() {
+    document.querySelector('.js-reset-confirmation').innerHTML = `
+    <div class="reset-confirm-wrapper">
+        <p>Are you sure you want to reset the score?</p>
+        <button class="js-reset-confirm-yes reset-confirm-button">Yes</button>
+        <button class="js-reset-confirm-no reset-confirm-button">No</button>
+    </div>
+`;
+
+
+    document.querySelector('.js-reset-confirm-yes').addEventListener('click', () => {
+        resetScore();
+        hideResetConfimation();
+    })
+
+    document.querySelector('.js-reset-confirm-no').addEventListener('click', () => {
+        hideResetConfimation();
+    })
+}
+
+function hideResetConfimation() {
+    document.querySelector('.js-reset-confirmation').innerHTML = '';
+}
