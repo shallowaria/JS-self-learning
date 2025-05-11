22a3cfee53079ffacac098bfa9c29ccbe6a28293
shallowaria <shallowaria@gmail.com>
2025-05-11 17:18:05 +0800

    Make add to fcart button active

--- JavaScript笔记/javascriptcourse/13.amazon-project and Git/scripts/amazon.js
+++ JavaScript笔记/javascriptcourse/13.amazon-project and Git/scripts/amazon.js
@@ -83,12 +83,37 @@ products.forEach((product) => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
             </button>
         </div>
     `;
-})
+});
 
 //3.put html in the page
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            cart.push({
+                productId,
+                quantity: 1
+            });
+        }
+
+        console.log(cart);
+    });
+});
