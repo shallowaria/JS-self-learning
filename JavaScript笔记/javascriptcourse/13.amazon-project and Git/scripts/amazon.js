d648ccf2ae46af1a18032d8b69bb73b363bbe603
shallowaria <shallowaria@gmail.com>
2025-05-13 14:41:13 +0800

    Fix radio selectors and transfrom price

--- JavaScript笔记/javascriptcourse/13.amazon-project and Git/scripts/amazon.js
+++ JavaScript笔记/javascriptcourse/13.amazon-project and Git/scripts/amazon.js
@@ -40,6 +40,7 @@
 import { cart, addToCart } from '../data/cart.js';
 //import * as cartModule from '../data/cart.js'; 另一种写法,导入时用cartModule.cart及cartModule.addToCart('id');
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
 //1.generate the HTML
@@ -64,7 +65,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
