76ab7d754937fccd9b6a06568008e4193584bcec
shallowaria <shallowaria@gmail.com>
2025-05-20 20:55:48 +0800

    Load products from the backend

--- JavaScript笔记/javascriptcourse/lesson-15/data/products.js
+++ JavaScript笔记/javascriptcourse/lesson-15/data/products.js
@@ -61,7 +61,27 @@ console.log(date);
 console.log(date.toLocaleTimeString());
 */
 
+// Load products from backend
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -727,7 +747,7 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
+*/
 //1.对象内部用this指向外部对象
 
 //2.对象外没有指向，是当前环境，this = undefined
