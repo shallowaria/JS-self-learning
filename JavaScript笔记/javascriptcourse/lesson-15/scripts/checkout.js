57dabe1d44e736f3b325275d17bbbfb0e1e1d239
shallowaria <shallowaria@gmail.com>
2025-05-21 17:42:16 +0800

    Convert from promises to async await

--- JavaScript笔记/javascriptcourse/lesson-15/scripts/checkout.js
+++ JavaScript笔记/javascriptcourse/lesson-15/scripts/checkout.js
@@ -6,6 +6,26 @@ import { loadCart } from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+//async = return a promise,只接受promise不接受callback
+async function loadPage() {
+    //await = .then(() => {});只能在async中使用
+    await loadProductsFetch();
+
+    const value = await new Promise((resolve) => {
+        loadCart(() => {
+            resolve('value3');
+        });
+    })
+
+    renderOrderSummary();
+    renderPaymentSummary();
+
+    // 此处return === resolve
+    //return 'value2';
+}
+loadPage()
+
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve) => {
@@ -19,6 +39,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
